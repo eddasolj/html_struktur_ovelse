6fce74e79efb5e93cd443866b55a0b22008781c6
eddasolj <124664752+eddasolj@users.noreply.github.com>
2023-08-24 11:04:10 +0200

    js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,6 @@
 const article1 = document.querySelector("article");
 console.log(article1);
 
-const toptitle = document.querySelector("h1");
-console.log(toptitle);
-
 const p_about_dog = document.querySelector(".trying_console");
 console.log(p_about_dog);
 
@@ -12,3 +9,11 @@ console.log(main);
 
 const footer = document.querySelector("footer");
 console.log(footer);
+
+const toptitle = document.querySelector("h1");
+console.log(toptitle);
+toptitle.textContent = "changed title"; /// to change text ////
+
+const test = document.querySelector("p");
+console.log(test);
+test.innerHTML = "<h4>nytekst</h4>"; /// to change element in i element ////
