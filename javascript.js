0ec4fb10828575f3a4a3e3b3c884c3b47dcc4200
eddasolj <124664752+eddasolj@users.noreply.github.com>
2023-08-24 10:28:49 +0200

    5 query selectors

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const article1 = document.querySelector("article");
+console.log(article1);
+
+const toptitle = document.querySelector("h1");
+console.log(toptitle);
+
+const p_about_dog = document.querySelector(".trying_console");
+console.log(p_about_dog);
+
+const main = document.querySelector("main");
+console.log(main);
+
+const footer = document.querySelector("footer");
+console.log(footer);
